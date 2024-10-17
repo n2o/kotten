4188ca7b01ae0460c551acb58f3c80d14fc49f6d
Christian Meter <cmeter@googlemail.com>
2024-10-18 00:35:26 +0200

    Add open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,43 @@
 import { Providers } from "@/app/providers"
 import { Navbar } from "@/components/navigation/navbar"
+import openGraphKotten from "@/images/opengraph.jpg"
 import { Box } from "@chakra-ui/react"
 import { Analytics } from "@vercel/analytics/react"
 import { GeistSans } from "geist/font/sans"
 import type { Metadata } from "next"
 
-export const metadata: Metadata = {
-  title: {
-    default: "Diederichskotten",
-    template: "%s | Diederichskotten",
-  },
-  description:
-    "Willkommen beim Diederichskotten, einem Stück Bergische Geschichte in Remscheid.",
+export async function generateMetadata(): Promise<Metadata> {
+  const title = "Diederichskotten"
+  const description =
+    "Willkommen beim Diederichskotten, einem Stück Bergische Geschichte in Remscheid."
+
+  const images = [
+    {
+      url: openGraphKotten.src,
+      width: 1200,
+      height: 630,
+      alt: "Diederichskotten in Remscheid",
+    },
+  ]
+
+  return {
+    title: {
+      default: title,
+      template: "%s | Diederichskotten",
+    },
+    description,
+    openGraph: {
+      title,
+      description,
+      images,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images,
+    },
+  }
 }
 
 export default function RootLayout({
