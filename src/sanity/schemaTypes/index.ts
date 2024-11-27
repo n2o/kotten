c27a548d96c26cee0fa805d2c02e934c6564af84
Christian Meter <cmeter@googlemail.com>
2024-11-27 22:39:53 +0100

    Add sentry

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,46 @@
+import { defineField, defineType, type SchemaTypeDefinition } from "sanity"
+
+export const eventType = defineType({
+  name: "gallery",
+  title: "Gallery",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+    }),
+    {
+      name: "images",
+      type: "array",
+      title: "Images",
+      of: [
+        {
+          name: "image",
+          type: "image",
+          title: "Image",
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: "alt",
+              type: "string",
+              title: "Alternative text",
+            },
+          ],
+        },
+      ],
+      options: {
+        layout: "grid",
+      },
+    },
+  ],
+})
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [eventType],
+}
