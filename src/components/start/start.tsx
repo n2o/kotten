0168fde57b35b0debcfac42947937b60a964f4c9
Christian Meter <cmeter@googlemail.com>
2025-07-01 22:59:20 +0200

    Modify logo

--- src/components/start/start.tsx
+++ src/components/start/start.tsx
@@ -1,20 +1,25 @@
 import { ContentCard } from "@/components/content-card"
 import { Post20250214, Wohnhaus } from "@/components/content-cards"
+import { Logo } from "@/components/logo"
 import { OktoberBauphase } from "@/components/oktober-bauphase"
 import { Articles } from "@/components/rga"
 import { Hero } from "@/components/start/hero"
-import { CardProps, Container, Text } from "@chakra-ui/react"
+import { CardProps, Container, HStack, Text } from "@chakra-ui/react"
 
 function Greeting(props: CardProps) {
   return (
     <ContentCard heading="Willkommen" hierarchy="h1" fontSize="3xl" {...props}>
-      <Text>
-        Der Diederichskotten ist ein denkmalgeschütztes Gebäude im Hammertal,
-        das seit den 1980er Jahren von den Remscheider Pfadfindern genutzt wird.
-        Im Jahr 1990 wurde ein Verein gegründet, um das Gebäude zu erhalten und
-        als Ort für bündische Veranstaltungen zu nutzen und so den Gruppen des
-        Deutschen Pfadfinderbundes in Remscheid eine Heimat bietet.
-      </Text>
+      <HStack>
+        <Text>
+          Der Diederichskotten ist ein denkmalgeschütztes Gebäude im Hammertal,
+          das seit den 1980er Jahren von den Remscheider Pfadfindern genutzt
+          wird. Im Jahr 1990 wurde ein Verein gegründet, um das Gebäude zu
+          erhalten und als Ort für bündische Veranstaltungen zu nutzen und so
+          den Gruppen des Deutschen Pfadfinderbundes in Remscheid eine Heimat
+          bietet.
+        </Text>
+        <Logo height="150px" width="150px" />
+      </HStack>
       <Articles />
     </ContentCard>
   )
